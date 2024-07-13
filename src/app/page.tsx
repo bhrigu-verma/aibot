ede19d06b66f01625b9accc282caf9f3e4ff91b4
Bhrigu verma <bhriguverma.bt23cse@pec.edu.in>
2024-07-13 13:48:57 +0530

    landing pag in process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-
+//accertinity imports
+"use client"
+import { GlareCard } from "@/components/ui/glare-card";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
+//accertinity imports
+// import Typewriter from 'typewriter-effect';
 import NavBar from '@/components/navbar';
 import { Button } from '@/components/ui/button';
 import Layout from './layout';
@@ -16,49 +22,102 @@ import { Check } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Suspense } from 'react';
+import { Typewriter } from 'react-simple-typewriter'
 
-export default async function Home() {
+export default function Home() {
+  const woords=[
+    {text:" Want",},
+    {text:" to",},
+    {text:" integrate ",},
+    {
+      text: "AI",
+      className: "text-blue-500 dark:text-blue-500",
+    }, {text:" in your",},{text:" website",},
+  ]
+  const words = [
+    {
+      text: "Agent ",
+    },
+    
+   
+    {
+      text: "Acumen",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+    {
+      text: "AI",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
   return (
    <div>
-    <Layout>
-      <Suspense fallback={<div>Loading Navbar...</div>}>
+    {/* <Layout>
+      <Suspense fallback={<div>Loading Navbar...</div>}> */}
     <main>
-   
-          <NavBar />
-        
-      <section>
-        <div className="flex items-center justify-center flex-col mt-[80px] gap-4">
-          <span className="text-orange bg-orange/20 px-4 py-2 rounded-full text-sm">
-            An AI powered sales assistant chatbot
-          </span>
-          <Image
-            src="/images/corinna-ai-logo.png"
-            width={500}
-            height={100}
-            alt="Logo"
-            className="max-w-lg object-contain"
-          />
-          <p className="text-center max-w-[500px]">
-            Your AI powered sales assistant! Embed Corinna AI into any website with just a snippet of code!
-          </p>
-          <Button className="bg-orange font-bold text-white px-4">Start For Free</Button>
-          <Image
-            src="/images/iphonecorinna.png"
-            width={400}
-            height={100}
-            alt="Logo"
-            className="max-w-lg object-contain"
-          />
-        </div>
-      </section>
 
+   <div className="flex gap-5 justify-between items-center px-7 py-1 font-bold border-b border-solid border-zinc-100 leading-[154.5%] max-md:flex-wrap max-md:px-5">
+      <div className="  flex gap-1.5 justify-center self-stretch my-auto text-2xl tracking-tighter text-neutral-200">
+        ACUMEN AI 
+      </div>
+      <ul className="gap-5 justify-between self-stretch my-auto text-sm leading-5 text-neutral-400 max-md:flex-wrap max-md:max-w-full font-normal hidden md:flex">
+        <Link href="/home">Home</Link>
+        <Link href="/home">Pricing</Link>
+        <Link href="/documentation">Documentation</Link>
+        <Link href="/home">Features</Link>
+        <Link href="/home">Contact us</Link>
+      </ul>
+    
+      <Link
+        href="/dashboard"
+        className="bg-orange px-4 py-2 rounded-sm text-white"
+      >
+        Free Trial
+      </Link>
+    </div>
+        
+    <section>
+      <div className="flex select-none mt-10 ">
+      <div className="w-[60%]  height-full">
+        
+        <div className="text-zinc-300 top-5 mt-[50px] ml-5 text-[95px] gap-1 font-bold">Your own AI 
+           <TypewriterEffectSmooth words={words} /></div>
+        <p className='text-zinc-300 top-5 mt-5 ml-5 text-lg font-medium'>Want to Integrate AI in your  <Typewriter
+          words={['Wordpress','html','Reactjs','NextApp']}
+          loop={false}
+          cursor
+          cursorStyle='_'
+          typeSpeed={50}
+          deleteSpeed={45}
+          delaySpeed={1000}
+        /> website? It's now a click away.</p>
+        <button className="w-40 m-6 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+          Join now
+        </button>
+        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Signup
+        </button>
+      </div>
+      <div className="w-[40%] mt-10">
+      <GlareCard className="flex flex-col items-center justify-center">
+
+      <p className="text-white font-bold  text-[40px] mt-4">Acumen Ai</p>
+    </GlareCard>
+    
+      </div></div>
+    </section>
+    <section className="flex mt-[15vw] justify-center">
+      <div><p>hello </p></div>
+
+
+    </section>
+{/* 
       <section className="flex justify-center items-center flex-col gap-4 mt-10">
         <h2 className="text-4xl text-center">Choose what fits you right</h2>
         <p className="text-muted-foreground text-center max-w-lg">
           Our straightforward pricing plans are tailored to meet your needs. If you're not ready to commit you can get started for free.
         </p>
-      </section>
-      <div className="flex  justify-center gap-4 flex-wrap mt-6">
+      </section> */}
+      {/* <div className="flex  justify-center gap-4 flex-wrap mt-6">
         {pricingCards.map((card) => (
           <Card
             key={card.title}
@@ -97,12 +156,12 @@ export default async function Home() {
                 Get Started
               </Link>
             </CardFooter>
-          </Card>
-        ))}
-      </div>
-      
-     </main>
-     </Suspense>
-     </Layout> </div>
+          </Card> 
+         ))}
+      </div> */}
+      </main>
+     {/* </Suspense> 
+    
+     </Layout>*/}  </div>  
   );
 }
--- src/components/chatbot/index.tsx
+++ src/components/chatbot/index.tsx
@@ -25,7 +25,7 @@ const AiChatBot = (props: Props) => {
   } = useChatBot()
 
   return (
-    <div className="h-screen flex flex-col justify-end items-end gap-4">
+    <div className="h-[100%] flex flex-col justify-end items-end gap-4">
       {botOpened && (
         <BotWindow
           errors={errors}
